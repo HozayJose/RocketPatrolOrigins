4cc5ccf2c78ca7ccb3157060f00640b6bdbb05c9
Jose Salas <jsalvadorsalas000@gmail.com>
2022-04-07 20:59:02 -0700

    commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,7 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20,20,"Main Menu");
+        let menuText = this.add.text(20,20,"Main Menu");
+        menuText.setOrigin(0.5, 0.5);
     }
 };

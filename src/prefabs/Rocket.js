ccfeae1c5263de5670bc546432ee710483c57b9d
Jose Salas <jsalvadorsalas000@gmail.com>
2022-04-08 16:45:26 -0700

    working rocket

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -4,5 +4,36 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         //adding object to the scene
         scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
+    }
+
+    update() {
+        //left right controls
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUIsize + this.width) {
+                this.x -= this.moveSpeed;
+                
+            } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUIsize - this.width) {
+                this.x += this.moveSpeed;
+                
+            }
+        }
+
+        //fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+            //console.log("fire");
+        }
+        //after firing rocket
+        if(this.isFiring && this.y >= borderUIsize*3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+
+        //if miss reset Y position
+        if(this.y <= borderUIsize*3 + borderPadding) {
+            this.isFiring = false;
+            this.y = game.config.height - borderUIsize - borderPadding;
+        }
     }
 }

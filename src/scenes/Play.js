e3db4b9483864cc6b3f5812de549c3e8d943296c
Jose Salas <jsalvadorsalas000@gmail.com>
2022-04-07 22:15:55 -0700

    made borders

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,5 +6,12 @@ class Play extends Phaser.Scene {
     create() {
         //green rectangle
         this.add.rectangle(0, borderPadding + borderUIsize, game.config.width, borderUIsize*2, 0x00FF00).setOrigin(0,0);
+    
+        //white borders for background
+        this.add.rectangle(0,0, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0, game.config.height - borderUIsize, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(game.config.width - borderUIsize, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    
     }
 }
